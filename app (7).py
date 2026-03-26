657e1bb085b242ddb147e57e99db34d21c1b3d39
jatinjatin42457 <jatinjatin4297@gmail.com>
2026-03-27 00:38:02 +0530

    Update app (7).py

--- app (7).py
+++ app (7).py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 import pandas as pd
-import plotly.express as px
+
 
 # --- 1. SETTINGS & STYLE ---
 st.set_page_config(page_title="Pro Portfolio Tracker", layout="wide", page_icon="📈")
